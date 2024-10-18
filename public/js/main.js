beef3697d8c1110e1a0189908e835e9423bfcaee
Lorenzo Fornara <fw1987@gmail.com>
2024-10-18 09:29:02 +0000

    add accordion

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,24 @@ const hamburger = document.querySelector("#hamburger-menu");
 const nav = document.querySelector(".u-nav-container");
 const fseffect = document.querySelector(".fseffect");
 
+const uaccordionpane = document.querySelectorAll(".u-accordion-pane");
+const uaccordionitem = document.querySelectorAll(".u-accordion-item");
+const uaccordionlink = document.querySelectorAll(".u-accordion-link");
+
 hamburger.addEventListener("click", () => {
   nav.classList.toggle("open");
   fseffect.classList.toggle("seffect");
+});
+
+uaccordionlink.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    uaccordionlink.forEach((link) => {
+      link.classList.remove("active");
+    });
+
+    uaccordionpane.forEach((panel) => {
+      panel.classList.remove("u-accordion-active");
+    });
+    uaccordionpane[index].classList.add("u-accordion-active");
+  });
 });
