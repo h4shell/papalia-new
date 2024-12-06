28d36a24224f19f05b2ef0cc5307f45ceacadc1a
Lorenzo Fornara <h4shell@gmail.com>
2024-12-06 11:38:28 +0000

    update

--- public/js/main.js
+++ public/js/main.js
@@ -4,22 +4,31 @@ const fseffect = document.querySelector(".fseffect");
 
 const uaccordionpane = document.querySelectorAll(".u-accordion-pane");
 const uaccordionitem = document.querySelectorAll(".u-accordion-item");
-const uaccordionlink = document.querySelectorAll(".u-accordion-link");
+const uaccordionlinks = document.querySelectorAll(".u-accordion-link");
 
 // hamburger.addEventListener("click", () => {
 //   nav.classList.toggle("open");
 //   fseffect.classList.toggle("seffect");
 // });
 
-uaccordionlink.forEach((item, index) => {
-  item.addEventListener("click", () => {
-    uaccordionlink.forEach((link) => {
+uaccordionlinks.forEach((link, index) => {
+  link.addEventListener("click", () => {
+    
+    if(uaccordionpane[index].classList.contains("u-accordion-active")){
+      link.classList.remove("active");
+      uaccordionpane[index].classList.remove("u-accordion-active");
+      console.log("active")
+      return
+    }
+
+    uaccordionlinks.forEach((link) => {
       link.classList.remove("active");
     });
 
     uaccordionpane.forEach((panel) => {
       panel.classList.remove("u-accordion-active");
     });
-    uaccordionpane[index].classList.add("u-accordion-active");
+    uaccordionpane[index].classList.toggle("u-accordion-active");
+    link.classList.toggle("active");
   });
 });
